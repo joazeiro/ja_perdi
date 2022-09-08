0728a61c5962ac39dfbe48ac614c298a9a7a9eb1
Duda <eduardajoazeiro@knights.ucf.edu>
2022-09-08 00:07:25 -0400

    parei pq a emily precisava de ajuda

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import sys, pygame
+import button
 
 pygame.init()
 
-size = width, height = 800, 600
-black = 0, 0, 0
+width = 800
+height = 600
+
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Not PhotoMath")
 
-screen = pygame.display.set_mode(size)
+font = pygame.font.SysFont("arialblack", 40)
+font_color = (255, 255, 255)
+
+def text(text, font, color, x, y):
+    img = font.render(text, True, color)
+    screen.blit(img, (x,y))
 
 while 1:
+    
+    screen.fill((52, 78, 91))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+
+
+    pygame.display.update()
+
